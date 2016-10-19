65dbb982d1d24f02c1b7e4b85508e81ab7cd0f71
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2016-10-20 00:16:27 +0530

    added count fucntions

--- loktra/crawler/scraper.py
+++ loktra/crawler/scraper.py
@@ -2,9 +2,8 @@ from __future__ import print_function
 from sys import argv
 from lxml import html
 import requests
-import time
-import multiprocessing
 import getopt
+import random
 
 # logging
 import logging
@@ -12,60 +11,23 @@ logging.basicConfig(filename='log/exception_logging.txt', level=logging.DEBUG, f
 logger=logging.getLogger(__name__)
 
 # constants
-MIN_PE_THRESHOLD = 0
-MAX_PE_THRESHOLD = 15
-MIN_EPS_THRESHOLD = 0
 COMPANY_URL = "http://www.shopping.com/products"
 
 
-def _get_company_primary_stats(company, tree):
-    pe_ratio = company.get_pe_ratio(tree)
-    eps = company.get_eps(tree)
-    price_of_stock = company.get_price_of_stock(tree)
-    fifty_two_wk_high = company.get_fifty_two_wk_high(tree)
-    fifty_two_wk_low = company.get_fifty_two_wk_low(tree)
-    return {'pe_ratio': pe_ratio,
-            'eps': eps,
-            'price_of_stock': price_of_stock,
-            'fifty_two_wk_high': fifty_two_wk_high,
-            'fifty_two_wk_low': fifty_two_wk_low}
+def results_count(page):
+    tree = html.fromstring(page.content)
+    upper = 100
+    lower = 1
+    pivot = random.randint(1, 100)
+    while True:
+        if len(tree.xpath(".//*[@id='nameQA%s']" % pivot)) > 0:
+            if len(tree.xpath(".//*[@id='nameQA%s']" % (pivot + 1))) == 0:
+                return pivot
+            else:
+                pivot += 1
+        else:
+            pivot -= 1
 
-
-def company_page_analysis(stock_company):
-    try:
-        page = requests.get('http://money.rediff.com/%s' % stock_company)
-        time.sleep(2)
-        tree = html.fromstring(page.content)
-        company = CompanyPage(tree)
-        primary_stats = _get_company_primary_stats(company, tree)
-        if all([primary_stats.get('pe_ratio') > MIN_PE_THRESHOLD, primary_stats.get('pe_ratio') < MAX_PE_THRESHOLD, primary_stats.get('eps') > MIN_EPS_THRESHOLD, primary_stats.get('price_of_stock') < ((primary_stats.get('fifty_two_wk_high') + primary_stats.get('fifty_two_wk_low'))/2)]):
-
-
-            #get all links
-            balance_sheet_link = company.get_balance_sheet_link(tree)
-            dividend_link = company.get_dividend_link(tree)
-            ratio_link = company.get_ratio_link(tree)
-
-            # go to balance sheet page for further analysis
-            balance_sheet_page = requests.get('%s' % ''.join(balance_sheet_link))
-            balance_sheet_tree = html.fromstring(balance_sheet_page.content)
-            balance_sheet = BalanceSheet(balance_sheet_tree)
-            current_assets_loans_advances = balance_sheet.get_current_assets_loans_advances(balance_sheet_tree)
-            current_liabilities_and_provisions = balance_sheet.get_current_liabilities_and_provisions(balance_sheet_tree)
-            total_net_current_assets = balance_sheet.get_total_net_current_assets(balance_sheet_tree)
-            if total_net_current_assets > current_liabilities_and_provisions:
-                # go to ratio page
-                ratio_page = requests.get('%s' % ''.join(ratio_link))
-                ratio_tree = html.fromstring(ratio_page.content)
-                ratio = Ratio(ratio_tree)
-                if ratio.consistent_dividend_payout(ratio_tree):
-                    print(stock_company)
-
-    except Exception as err:
-        logger.error(err)
-
-
-    return
 
 def crawler(keyword, page_number=None):
     global COMPANY_URL
@@ -75,9 +37,10 @@ def crawler(keyword, page_number=None):
                                 (COMPANY_URL, page_number, keyword))
         page = requests.get('%s?KW=%s' %
                             (COMPANY_URL, keyword))
-        print(page)
+        return results_count(page)
     except Exception as err:
         logger.error(err)
+
 
 def main(argv):
     try:
