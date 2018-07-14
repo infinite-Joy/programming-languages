f20e01697b09a8e295201125652bcc428f601235
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2018-07-14 12:58:38 +0530

    guessing game

--- rust-lang/guessing_game/src/main.rs
+++ rust-lang/guessing_game/src/main.rs
@@ -10,17 +10,24 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guesses");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
-    print!("You guessed: {}", guess);
+    loop {
+        println!("Please input your guesses");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        print!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
