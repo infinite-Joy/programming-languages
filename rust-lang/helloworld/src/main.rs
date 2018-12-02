c1962611bcd706508c8167259fdbc3681f55b615
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2018-12-02 12:01:19 +0530

    for loops.

--- rust-lang/helloworld/src/main.rs
+++ rust-lang/helloworld/src/main.rs
@@ -1,3 +1,53 @@
+use std::ops:: Add;
+
+fn first<T>(slice: &[T]) -> &T {
+    &slice[0]
+
+}
+
+fn max<T: PartialOrd>(a: T, b: T) -> T {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
+
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+trait BitSet {
+    fn clear(&mut self, index: usize);
+    fn is_set(&self, index: usize) -> bool;
+    fn set(&mut self, index: usize);
+
+    fn toggle(&mut self, index: usize) {
+        if self.is_set(index) {
+            println!("will clear {}", index);
+            self.clear(index);
+        } else {
+            println!("will set {}", index);
+            self.set(index);
+        }
+        
+    }
+    
+}
+
+impl BitSet for u64 {
+    fn clear(&mut self, index: usize) {
+        *self &= !(1 << index);
+    }
+    fn is_set(&self, index: usize) -> bool {
+        (*self >> index) & 1 == 1
+    }
+    fn set(&mut self, index: usize) {
+        *self |= 1 << index;
+    }
+}
+
 fn upppercase(c: u8) -> u8 {
     match c {
         b'a'...b'z' => c - 32,
@@ -40,13 +90,13 @@ fn inc_x(point: &mut Point) {
     point.x += 1;
 }
 
-fn max(a: i32, b: i32) -> i32 {
-    if a > b {
-        a
-    } else {
-        b
-    }
-}
+//fn max(a: i32, b: i32) -> i32 {
+//    if a > b {
+//        a
+//    } else {
+//        b
+//    }
+//}
 
 fn print_point(point: &Point) {
     println!("This is inside the function");
@@ -72,6 +122,17 @@ impl Point {
     fn translate(&mut self, dx: i32, dy: i32) {
         self.x += dx;
         self.y += dy;
+    }
+}
+
+impl Add<Point> for Point {
+    type Output = Point;
+
+    fn add(self, point: Point) -> Self::Output {
+        Point {
+            x: self.x + point.x,
+            y: self.y + point.y,
+        }
     }
 }
 
@@ -138,7 +199,39 @@ fn main() {
     print_expr(sum);
 
     println!("{}", upppercase(b'j') as char);
-    println!("{}", is_alphanumeric('j') as u8);
+    println!("{}", is_alphanumeric('j'));
+
+    println!("Trying out traits.");
+    let mut num = 0;
+    num.set(15);
+    println!("{}", num.is_set(15));
+    num.clear(15);
+    num.toggle(16);
+    num.toggle(16);
+    println!("done with the traits.");
+
+    println!("Associated types");
+    let p1 = Point {x: 1, y: 2};
+    let p2 = Point {x: 3, y: 4};
+    let p3 = p1 + p2;
+    println!("({}, {})", p3.x, p3.y);
+    println!("{}", max('a', 'z'));
+
+    let array: [i16; 4] = [1, 2, 3, 4];
+    println!("{}", array[3]);
+
+    println!("Slices are fat pointers");
+    println!("first of the array {}", first(&array));
+    println!("first of the array {}", first(&array[2..]));
+
+    println!("for loops");
+    let array: [i16; 4] = [1, 2, 3, 4];
+    let mut sum = 0;
+    for element in &array {
+        sum += *element;
+    }
+    println!("Sum: {}", sum);
+
 
 
 }
