d76703684c902b62fec03be40fbe929cf3626614
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2018-12-26 12:23:08 +0530

    add support for gzip files.

--- rust-lang/smart-open/src/lib.rs
+++ rust-lang/smart-open/src/lib.rs
@@ -13,11 +13,33 @@ use std::io::{self, Read};
 use std::num;
 
 pub fn smart_open(filepath: &str) -> std::io::Result<String> {
-    let file = File::open(filepath)
-        .expect("Unable to open the file");
-    let mut buf_reader = BufReader::new(file);
+    // TODO: make the function more modular.
+    let path = Path::new(filepath);
+    let extension = path.extension();
+    let text_ext = OsStr::new("gz");
+    let gz_ext = OsStr::new("txt");
+    // println!("{:#?}", extension);
+    let content_type = match path.extension() {
+        None => panic!("Paths without extension is not allowed!!"),
+        Some(os_str) => {
+            match os_str.to_str() {
+                Some("txt") => "text",
+                Some("gz") => "gz",
+                Some(e) => panic!("{} case is not implemented yet. Please contact the developers!", e),
+                None => panic!("None has seeped in somehow. Please contact developers."),
+            }
+        }
+    };
+    let file_handler = File::open(&path)?;
+    let mut buf_reader = BufReader::new(file_handler);
     let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)
-        .expect("Unable to read the file");
+    if content_type == "gz" {
+        let mut reader = GzDecoder::new(buf_reader);
+        reader.read_to_string(&mut contents).unwrap();
+    } else if content_type == "text" {
+        buf_reader.read_to_string(&mut contents)?;
+    } else {
+        panic!("Content type {} is not allowed.", content_type);
+    }
     Ok(contents.trim().to_string())
 }
--- rust-lang/smart-open/tests/lib.rs
+++ rust-lang/smart-open/tests/lib.rs
@@ -3,14 +3,24 @@ extern crate smart_open as sm;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_gzip_file() {
-        // assert_eq!(sm::smart_open("/Users/joydeep/thinking/programming-languages/rust-lang/smart-open/foo.txt").unwrap(), "Hello, world!");
+    fn test_simple_text_file() {
         assert_eq!(sm::smart_open("tests/foo.txt").unwrap(), "Hello, world!");
     }
 
     #[test]
-    #[should_panic(expected = r#"No such file or directory"#)]
+    #[should_panic(expected = r#"without extension"#)]
     fn test_gzip_file_invalidfile() {
         let _ = sm::smart_open("somefile");
+    }
+
+    #[test]
+    fn test_gzip_file() {
+        assert_eq!(sm::smart_open("tests/bar.txt.gz").unwrap(), "Hello, world!");
+    }
+
+    #[test]
+    #[should_panic(expected = r#"png case is not implemented yet"#)]
+    fn test_unknown_file() {
+        let _ = sm::smart_open("tests/abc.png");
     }
 }
