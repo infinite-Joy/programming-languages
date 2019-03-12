77b5cd928e4efa0bfad4ffff61570d415469bfbe
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2019-03-12 22:30:14 +0530

    formatting app.py.

--- python-projects/mandal-project/app.py
+++ python-projects/mandal-project/app.py
@@ -7,7 +7,7 @@
 #  10Jan18
 
 '''
-	RPi WEb Server for DHT captured data with Graph plot
+    RPi WEb Server for DHT captured data with Graph plot
 '''
 
 
@@ -24,50 +24,49 @@ curs=conn.cursor()
 
 # Retrieve LAST data from database
 def getLastData():
-	for row in curs.execute("select * from voltage_current order by timestamp desc LIMIT 1;"):
-		voltage = row[1]
-		current = row[2]
-		time = str(row[3])
-	#conn.close()
-	return voltage, current, time
+    for row in curs.execute("select * from voltage_current order by timestamp desc LIMIT 1;"):
+        voltage = row[1]
+        current = row[2]
+        time = str(row[3])
+    #conn.close()
+    return voltage, current, time
 
 
-def getHistData (numSamples):
-	curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT "+str(numSamples))
-	data = curs.fetchall()
-	dates = []
-	temps = []
-	hums = []
-	for row in reversed(data):
-		dates.append(row[0])
-		temps.append(row[1])
-		hums.append(row[2])
-	return dates, temps, hums
+def getHistData(numSamples=10000):
+    curs.execute("SELECT * FROM voltage_current ORDER BY timestamp DESC LIMIT "+str(numSamples))
+    data = curs.fetchall()
+    dates = []
+    temps = []
+    hums = []
+    for row in reversed(data):
+        dates.append(row[0])
+        temps.append(row[1])
+        hums.append(row[2])
+    return dates, temps, hums
 
 def maxRowsTable():
-	for row in curs.execute("select COUNT(temp) from  DHT_data"):
-		maxNumberRows=row[0]
-	return maxNumberRows
+    for row in curs.execute("select COUNT(*) from  voltage_current"):
+        maxNumberRows=row[0]
+    return maxNumberRows
 
 #initialize global variables
-global numSamples
+numSamples
 numSamples = maxRowsTable()
 if (numSamples > 101):
-	numSamples = 100
+    numSamples = 100
 
 
 # main route
 @app.route("/")
 def index():
-
-	time, temp, hum = getLastData()
-	templateData = {
-	  'time'		: time,
-      'temp'		: temp,
-      'hum'			: hum,
-      'numSamples'	: numSamples
-	}
-	return render_template('index.html', **templateData)
+    time, temp, hum = getLastData()
+    templateData = {
+      'time'       : time,
+      'temp'       : temp,
+      'hum'        : hum,
+      'numSamples' : numSamples
+    }
+    return render_template('index.html', **templateData)
 
 
 @app.route('/', methods=['POST'])
@@ -81,49 +80,49 @@ def my_form_post():
     time, temp, hum = getLastData()
 
     templateData = {
-	  'time'		: time,
-      'temp'		: temp,
-      'hum'			: hum,
-      'numSamples'	: numSamples
-	}
+      'time'       : time,
+      'temp'       : temp,
+      'hum'        : hum,
+      'numSamples' : numSamples
+    }
     return render_template('index.html', **templateData)
 
 
 @app.route('/plot/temp')
 def plot_temp():
-	times, temps, hums = getHistData(numSamples)
-	ys = temps
-	fig = Figure()
-	axis = fig.add_subplot(1, 1, 1)
-	axis.set_title("Temperature [°C]")
-	axis.set_xlabel("Samples")
-	axis.grid(True)
-	xs = range(numSamples)
-	axis.plot(xs, ys)
-	canvas = FigureCanvas(fig)
-	output = io.BytesIO()
-	canvas.print_png(output)
-	response = make_response(output.getvalue())
-	response.mimetype = 'image/png'
-	return response
+    times, temps, hums = getHistData(numSamples)
+    ys = temps
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    axis.set_title("Temperature [°C]")
+    axis.set_xlabel("Samples")
+    axis.grid(True)
+    xs = range(numSamples)
+    axis.plot(xs, ys)
+    canvas = FigureCanvas(fig)
+    output = io.BytesIO()
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
 
 @app.route('/plot/hum')
 def plot_hum():
-	times, temps, hums = getHistData(numSamples)
-	ys = hums
-	fig = Figure()
-	axis = fig.add_subplot(1, 1, 1)
-	axis.set_title("Humidity [%]")
-	axis.set_xlabel("Samples")
-	axis.grid(True)
-	xs = range(numSamples)
-	axis.plot(xs, ys)
-	canvas = FigureCanvas(fig)
-	output = io.BytesIO()
-	canvas.print_png(output)
-	response = make_response(output.getvalue())
-	response.mimetype = 'image/png'
-	return response
+    times, temps, hums = getHistData(numSamples)
+    ys = hums
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    axis.set_title("Humidity [%]")
+    axis.set_xlabel("Samples")
+    axis.grid(True)
+    xs = range(numSamples)
+    axis.plot(xs, ys)
+    canvas = FigureCanvas(fig)
+    output = io.BytesIO()
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=False)
