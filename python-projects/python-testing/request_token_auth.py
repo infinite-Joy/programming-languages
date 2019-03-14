69a17c384f7424a9c4d568963520db1d5e48dcfc
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2019-03-14 09:53:40 +0530

    request auth token example.

---
+++ python-projects/python-testing/request_token_auth.py
@@ -0,0 +1,16 @@
+import requests
+from requests.auth import AuthBase
+
+class TokenAuth(AuthBase):
+    """Implements a custom authentication scheme."""
+
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        """Attach an API token to a custom auth header."""
+        r.headers['X-TokenAuth'] = f'{self.token}'  # Python 3.6+
+        return r
+
+
+requests.get('https://httpbin.org/get', auth=TokenAuth('12345abcde-token'))
