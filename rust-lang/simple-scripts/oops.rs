32b5be2697b60c99de0c4aa0bcfb4c6a24bcf2a9
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2019-03-16 14:22:38 +0530

    traits.

--- rust-lang/simple-scripts/oops.rs
+++ rust-lang/simple-scripts/oops.rs
@@ -1,5 +1,5 @@
-// structures.rs
-// $ ./structures
+// oops.rs
+// $ ./oops
 // Planet { co2: 0.04, nitrogen: 78.09 }
 // Planet { co2: 95.32, nitrogen: 2.7 }
 // For planet Planet { co2: 0.04, nitrogen: 78.09 }: co2 = 0.04, nitrogen=78.09, other_gases=21.870003
@@ -12,25 +12,25 @@ struct Planet {
 }
 
 trait Atmosphere {
-	fn new(co2: f32, nitrogen: f32) -> Self;
-	fn amount_of_other_gases(&self) -> f32;
-	fn summarize(&self);
+    fn new(co2: f32, nitrogen: f32) -> Self;
+    fn amount_of_other_gases(&self) -> f32;
+    fn summarize(&self);
 }
 
 impl Atmosphere for Planet {
-	fn new(co2: f32, nitrogen: f32) -> Planet {
-		Planet { co2: co2, nitrogen: nitrogen }
-	}
+    fn new(co2: f32, nitrogen: f32) -> Planet {
+        Planet { co2: co2, nitrogen: nitrogen }
+    }
 
-	fn amount_of_other_gases(&self) -> f32 {
-		100.0 - self.co2 - self.nitrogen
-	}
+    fn amount_of_other_gases(&self) -> f32 {
+        100.0 - self.co2 - self.nitrogen
+    }
 
-	fn summarize(&self) {
-		let other_gases = self.amount_of_other_gases();
-		println!("For planet {planet:?}: co2 = {co2}, nitrogen={nitrogen}, other_gases={other_gases}",
-			planet=self, co2=self.co2, nitrogen=self.nitrogen, other_gases=other_gases);
-	}
+    fn summarize(&self) {
+        let other_gases = self.amount_of_other_gases();
+        println!("For planet {planet:?}: co2 = {co2}, nitrogen={nitrogen}, other_gases={other_gases}",
+            planet=self, co2=self.co2, nitrogen=self.nitrogen, other_gases=other_gases);
+    }
 }
 
 fn main() {
