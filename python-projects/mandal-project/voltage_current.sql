b7336e36ba20035ceb840f298bef4b0921215fab
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2019-03-18 13:25:58 +0530

    use utc epoch timestamp in db.

--- python-projects/mandal-project/voltage_current.sql
+++ python-projects/mandal-project/voltage_current.sql
@@ -1,6 +1,6 @@
 drop table if exists voltage_current;
 
-create table voltage_current (ID INTEGER PRIMARY KEY, voltage real, current real, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
+create table voltage_current (ID INTEGER PRIMARY KEY, voltage real, current real, Timestamp DATETIME DEFAULT strftime('%s', 'now'));
 
 insert into voltage_current (ID, voltage, current) values (1, 1.0, 2.0);
 insert into voltage_current (ID, voltage, current) values (2, 2.0, 4.0);
