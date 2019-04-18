037f2ed424b83aebc0a9da0ce5f15963687e8e98
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2019-04-18 11:09:06 +0530

    scraping | test the book value.

--- rust-lang/working_with_data/scraping/src/main.rs
+++ rust-lang/working_with_data/scraping/src/main.rs
@@ -14,7 +14,8 @@ fn main() -> Result<(), Box<std::error::Error>> {
 
     let body = resp.text().unwrap();
     let fragment = Html::parse_document(&body);
-    let stories = Selector::parse("#Bse_Prc_tick > strong:nth-child(1)").unwrap();
+    // let stories = Selector::parse("#Bse_Prc_tick > strong:nth-child(1)").unwrap();
+    let stories = Selector::parse("#mktdet_2 > div:nth-child(2) > div:nth-child(3) > div.FR.gD_12").unwrap();
 
     for price in fragment.select(&stories) {
         let price_txt = price.text().collect::<Vec<_>>();
