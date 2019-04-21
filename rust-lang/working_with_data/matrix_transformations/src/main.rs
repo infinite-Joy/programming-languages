920e221f118891b9c763e3f3ff825fb8176f8791
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2019-04-21 21:16:33 +0530

    data | load vector to ndarray.

--- rust-lang/working_with_data/matrix_transformations/src/main.rs
+++ rust-lang/working_with_data/matrix_transformations/src/main.rs
@@ -1,3 +1,12 @@
+use ndarray;
+use ndarray::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let a1 = arr2(&[[0., 1., 2.],
+                    [3., 4., 5.]]);
+    println!("{:?}", a1);
+
+    let a2 = Array::from_shape_vec((2, 3).strides((3, 1)),
+        vec![0., 1., 2., 3., 4., 5.]).unwrap();
+    assert!(a1 == a2);
 }
