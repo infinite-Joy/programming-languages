3d6b99abc9cc102bc84c95d4883d0727aefdd968
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2019-05-08 17:54:18 +0530

    cv | testing it on the directory with the new code.

--- rust-lang/chapter6/pytorch-image-classification/src/main.rs
+++ rust-lang/chapter6/pytorch-image-classification/src/main.rs
@@ -25,6 +25,8 @@ const C: i64 = 3;
 const BYTES_PER_IMAGE: i64 = W * H * C + 1;
 const SAMPLES_PER_FILE: i64 = 10000;
 
+const DATASET_FOLDER: &str = "dataset";
+
 // one possible implementation of walking a directory only visiting files
 fn create_train_val_folders(dir: &Path, train_fn: &Fn(&DirEntry), test_fn: &Fn(&DirEntry)) -> io::Result<()> {
     if dir.is_dir() {
@@ -38,6 +40,8 @@ fn create_train_val_folders(dir: &Path, train_fn: &Fn(&DirEntry), test_fn: &Fn(&
                 let full_path: Vec<String> = path.to_str().unwrap()
                     .split("/").into_iter()
                     .map(|x| x[..].to_string()).collect();
+                println!("{:?}", this_label);
+                println!("{:?}", full_path);
                 if this_label == full_path[1] {
                     train_fn(&entry); // move the training file
                 } else {
@@ -55,15 +59,16 @@ fn print_directory(dir: &Path) {
 }
 
 fn move_file(from_path: &DirEntry, to_path: &Path) -> io::Result<()> {
-    let highest_folder = Path::new("dataset");
+    let highest_folder = Path::new(DATASET_FOLDER);
     let second_order = highest_folder.join(to_path);
-    let full_path: Vec<String> = from_path.path().to_str().unwrap().split("/").into_iter().map(|x| x[..].to_string()).collect();
+    let full_path: Vec<String> = from_path.path().to_str().unwrap()
+        .split("/").into_iter().map(|x| x[..].to_string()).collect();
     let label = full_path[1].clone();
     let third_order = second_order.join(label);
     create_dir_all(&third_order)?;
     let filename = from_path.file_name();
     let to_filename = third_order.join(&filename);
-    println!("{:?}", to_filename);
+    // println!("{:?}", to_filename);
     copy(from_path.path(), to_filename)?;
     Ok(())
 }
@@ -93,16 +98,22 @@ fn main() {
     // let image_tensor = load_dir("test_image_dir", 50, 50).unwrap();
     // let image_dataset = load_from_dir("test_image_dir").unwrap();
     // println!("{:?}", image_dataset);
-    let obj_categories = Path::new("101_ObjectCategories");
-    let move_to_train = |x: &DirEntry| {
-        let to_folder = Path::new("train");
-        move_file(&x, &to_folder).unwrap();
-    };
-    let move_to_test = |x: &DirEntry| {
-        let to_folder = Path::new("val");
-        move_file(&x, &to_folder).unwrap();
-    };
 
-    create_train_val_folders(&obj_categories, &move_to_train, &move_to_test).unwrap();
+
+
+
+    // let obj_categories = Path::new("101_ObjectCategories");
+    // let obj_categories = Path::new("test_folder");
+    // let move_to_train = |x: &DirEntry| {
+    //     let to_folder = Path::new("train");
+    //     move_file(&x, &to_folder).unwrap();
+    // };
+    // let move_to_test = |x: &DirEntry| {
+    //     let to_folder = Path::new("val");
+    //     move_file(&x, &to_folder).unwrap();
+    // };
+    // create_train_val_folders(&obj_categories, &move_to_train, &move_to_test).unwrap();
+    let image_dataset = load_from_dir(DATASET_FOLDER).unwrap();
+    println!("{:?}", image_dataset);
     println!("Hello, world!");
 }
