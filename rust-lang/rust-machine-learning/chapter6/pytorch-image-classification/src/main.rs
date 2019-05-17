ce7c5483de1e7d2301dbde5fe2e4009049907140
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2019-05-17 12:04:58 +0530

    computer vision | dont need so much print statements.

--- rust-lang/rust-machine-learning/chapter6/pytorch-image-classification/src/main.rs
+++ rust-lang/rust-machine-learning/chapter6/pytorch-image-classification/src/main.rs
@@ -96,47 +96,47 @@ impl CnnNet {
     }
 }
 
-impl nn::ModuleT for CnnNet {
-   fn forward_t(&self, xs: &Tensor, train: bool) -> Tensor {
-       let xs_prime = xs.view(&[-1, C, H, W]);
-       println!("{:?}", xs_prime.size());
-       let xs_prime = xs_prime.apply(&self.conv1);
-       println!("{:?}", xs_prime.size());
-       let xs_prime = xs_prime.max_pool2d_default(2);
-       println!("{:?}", xs_prime.size());
-       let xs_prime = xs_prime.apply(&self.conv2);
-       println!("{:?}", xs_prime.size());
-       let xs_prime = xs_prime.max_pool2d_default(2);
-       println!("{:?}", xs_prime.size());
-       let xs_prime = xs_prime.view(&[BATCH_SIZE, -1]);
-    //    let xs_prime = xs_prime.view(&[-1, 1024]);
-       println!("{:?}", xs_prime.size());
-       let xs_prime = xs_prime.apply(&self.fc1);
-       println!("{:?}", xs_prime.size());
-       let mut xs_prime = xs_prime.relu();
-       println!("{:?}", xs_prime.size());
-       let xs_prime = xs_prime.dropout_(0.5, train);
-       println!("{:?}", xs_prime.size());
-       let xs_prime = xs_prime.apply(&self.fc2);
-       println!("{:?}", xs_prime.size());
-       xs_prime
-   }
-}
-
 // impl nn::ModuleT for CnnNet {
-//     fn forward_t(&self, xs: &Tensor, train: bool) -> Tensor {
-//         xs.view(&[-1, C, H, W])
-//             .apply(&self.conv1)
-//             .max_pool2d_default(2)
-//             .apply(&self.conv2)
-//             .max_pool2d_default(2)
-//             .view(&[-1, 1024])
-//             .apply(&self.fc1)
-//             .relu()
-//             .dropout_(0.5, train)
-//             .apply(&self.fc2)
-//     }
+//    fn forward_t(&self, xs: &Tensor, train: bool) -> Tensor {
+//        let xs_prime = xs.view(&[-1, C, H, W]);
+//        println!("{:?}", xs_prime.size());
+//        let xs_prime = xs_prime.apply(&self.conv1);
+//        println!("{:?}", xs_prime.size());
+//        let xs_prime = xs_prime.max_pool2d_default(2);
+//        println!("{:?}", xs_prime.size());
+//        let xs_prime = xs_prime.apply(&self.conv2);
+//        println!("{:?}", xs_prime.size());
+//        let xs_prime = xs_prime.max_pool2d_default(2);
+//        println!("{:?}", xs_prime.size());
+//        let xs_prime = xs_prime.view(&[BATCH_SIZE, -1]);
+//     //    let xs_prime = xs_prime.view(&[-1, 1024]);
+//        println!("{:?}", xs_prime.size());
+//        let xs_prime = xs_prime.apply(&self.fc1);
+//        println!("{:?}", xs_prime.size());
+//        let mut xs_prime = xs_prime.relu();
+//        println!("{:?}", xs_prime.size());
+//        let xs_prime = xs_prime.dropout_(0.5, train);
+//        println!("{:?}", xs_prime.size());
+//        let xs_prime = xs_prime.apply(&self.fc2);
+//        println!("{:?}", xs_prime.size());
+//        xs_prime
+//    }
 // }
+
+impl nn::ModuleT for CnnNet {
+    fn forward_t(&self, xs: &Tensor, train: bool) -> Tensor {
+        xs.view(&[-1, C, H, W])
+            .apply(&self.conv1)
+            .max_pool2d_default(2)
+            .apply(&self.conv2)
+            .max_pool2d_default(2)
+            .view(&[-1, 1024])
+            .apply(&self.fc1)
+            .relu()
+            .dropout_(0.5, train)
+            .apply(&self.fc2)
+    }
+}
 
 fn learning_rate(epoch: i64) -> f64 {
     if epoch < 10 {
