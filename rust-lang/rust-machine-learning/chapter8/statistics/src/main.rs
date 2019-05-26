308ab20b78bc45f26525b65b68c00c92f079766d
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2019-05-26 17:30:03 +0530

    others | statistics - process gene expression data.

--- rust-lang/rust-machine-learning/chapter8/statistics/src/main.rs
+++ rust-lang/rust-machine-learning/chapter8/statistics/src/main.rs
@@ -8,19 +8,29 @@ use std::collections::HashMap;
 
 // https://nbviewer.jupyter.org/urls/umich.box.com/shared/static/7kh8amlez7bx3qlqa6aa.ipynb?create=1
 
-fn process_gene_expresssion_data(thisline: &String, SIF: &HashMap<String, String>) {
+fn process_gene_expresssion_data_headers(thisline: &String, SIF: &HashMap<String, String>) -> (Vec<String>, Vec<String>, Vec<usize>) {
     println!(" this line: {:?}", thisline);
     let SID: Vec<String> = thisline.split("\t").map(|s| s.to_owned()).collect();
-    println!(" sid: {:?}", SID);
-    let indices = SID.iter()
+    let indices: Vec<usize> = SID.iter()
                 .enumerate()
                 .filter(|&(_, x)| x.starts_with("GSM") )
-                .map(|(i, _)| i);
-    let SID = indices.map(|i| SID[i].clone());
-    let STP: Vec<String> = SID.map(
+                .map(|(i, _)| i).collect();
+    let SID: Vec<String> = indices.iter().map(|&i| SID[i].clone()).collect();
+    println!(" sid: {:?}", SID);
+    let STP: Vec<String> = SID.iter().map(
         |k| SIF.get(&k.to_string()).unwrap())
         .cloned().collect();
     println!(" STP: {:?}", STP);
+    (SID, STP, indices)
+}
+
+/// Read the gene expression data as a list of lists, also get the gene identifiers
+fn process_gene_expresssion_data(thisline: &String, indices: &Vec<usize>, SIF: &HashMap<String, String>) -> (Vec<String>, String) {
+    let gene_expression_measure_values: Vec<String> = thisline.split("\t")
+        .map(|s| s.to_owned()).collect();
+    let gene_expression_measures: Vec<String> = indices.iter().map(|&i| gene_expression_measure_values[i].clone()).collect();
+    let gene_identifiers = vec![gene_expression_measure_values[0].clone(), gene_expression_measure_values[1].clone()].join(";");
+    (gene_expression_measures, gene_identifiers)
 }
 
 
@@ -28,18 +38,35 @@ fn process_file(filename: &Path) -> io::Result<HashMap<String, String>> {
     let mut SIF = HashMap::new();
     let file = File::open(filename).unwrap();
     let mut subset_description = String::new();
-    let mut after_dataset_table_begin = false;
-    for line in BufReader::new(file).lines() {
+    let mut within_dataset_table = false;
+    let mut within_headers = true;
+    let mut gene_expression_headers = true;
+    let mut indices: Vec<usize> = Vec::new();
+    'linereading: for line in BufReader::new(file).lines() {
         let thisline = line?;
         let line_split: Vec<String> = thisline.split("=").map(|s| s.to_owned()).collect();
         println!("this line: {:?}", thisline);
         if thisline.starts_with("!dataset_table_begin") {
-            after_dataset_table_begin = true;
+            within_dataset_table = true;
+            within_headers = false;
         }
-        if after_dataset_table_begin {
-            process_gene_expresssion_data(&thisline, &SIF);
-
-        } else {
+        // let mut SID: Vec<String> = Vec::new();
+        // let mut SID: Vec<String> = Vec::new();
+        // let mut SID: Vec<usize> = Vec::new();
+        if within_dataset_table && gene_expression_headers {
+            let sid_stp_indices = process_gene_expresssion_data_headers(&thisline, &SIF);
+            // SID, STP, indices = sid
+            indices = sid_stp_indices.2.clone();
+            gene_expression_headers = false;
+        };
+        println!("indices: {:?}", indices);
+        if within_dataset_table && !gene_expression_headers {
+            if thisline.starts_with("!dataset_table_end") {
+                break 'linereading;
+            }
+            process_gene_expresssion_data(&thisline, &indices, &SIF);
+        }
+        if within_headers {
             if thisline.starts_with("!subset_description") {
                 subset_description = line_split[1].trim().to_owned();
             };
