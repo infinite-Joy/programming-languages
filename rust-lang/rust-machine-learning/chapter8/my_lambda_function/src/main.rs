ddc82c6d64d414a43943b0e14f6d865fefecfd9f
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2019-06-15 11:21:20 +0530

    chapter8 | lambda - bring in the xgboost test function and write the test.

--- rust-lang/rust-machine-learning/chapter8/my_lambda_function/src/main.rs
+++ rust-lang/rust-machine-learning/chapter8/my_lambda_function/src/main.rs
@@ -13,6 +13,46 @@ use regex;
 use regex::Regex;
 use simple_error::bail;
 
+use csv::ReaderBuilder;
+use xgboost;
+use xgboost::{parameters, DMatrix, Booster};
+
+use ml_utils::datasets::Flower;
+
+fn flower_decoder(item: f32) -> String {
+    match item as i32 {
+        0 => "setosa".to_string(),
+        1 => "versicolor".to_string(),
+        2 => "virginica".to_string(),
+        l => panic!("Not able to parse the target. Some other target got passed. {:?}", l),
+    }
+}
+
+fn predict(data: String) -> Result<String, Box<dyn Error>> {
+    println!("Loading model");
+    let booster = Booster::load("xgb.model")?;
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(b',')
+        .from_reader(data.as_bytes());
+    let mut data = Vec::new();
+    for result in rdr.deserialize() {
+        let r: Flower = result.unwrap();
+        data.push(r); // data contains all the records
+    }
+    let val_size: usize = data.len();
+
+    // differentiate the features and the labels.
+    let flower_x_val: Vec<f32> = data.iter().flat_map(|r| r.into_feature_vector()).collect();
+    let flower_y_val: Vec<f32> = data.iter().map(|r| r.into_labels()).collect();
+
+    // validation matrix with 1 row
+    let mut dval = DMatrix::from_dense(&flower_x_val, val_size).unwrap();
+    dval.set_labels(&flower_y_val).unwrap();
+
+    let preds = booster.predict(&dval).unwrap();
+    Ok(flower_decoder(preds[0]))
+}
+
 #[derive(Serialize, Deserialize)]
 struct CustomEvent {
     string: String,
@@ -46,4 +86,31 @@ fn my_handler(event: CustomEvent, ctx: Context) -> Result<String, HandlerError> 
     let j = serde_json::to_string(&map).unwrap();
 
     Ok(j)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use csv;
+    use std::io;
+
+
+    // #[test]
+    // fn test_predict() {
+    //     let data = "sepal_length,sepal_width,petal_length,petal_width,species\n5.1,3.5,1.4,0.2,setosa\n";
+    //     let mut rdr = csv::Reader::from_reader(data.as_bytes());
+    //     let mut data = Vec::new();
+    //     for result in rdr.deserialize() {
+    //         let r: Flower = result.unwrap();
+    //         data.push(r); // data contains all the records
+    //     }
+    //     assert_eq!(data[0].sepal_length, 5.1);
+    // }
+    #[test]
+    fn test_predict() {
+        let data = "sepal_length,sepal_width,petal_length,petal_width,species\n5.1,3.5,1.4,0.2,setosa\n";
+        let res = predict(data.to_string());
+        assert_eq!(res.unwrap(), "setosa".to_string());
+    }
 }
