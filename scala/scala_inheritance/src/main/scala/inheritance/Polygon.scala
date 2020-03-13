902149f6ea516c0101d0db99c3c08b148a75ddcd
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-03-13 19:40:20 +0530

    scala abstract classes

--- scala/scala_inheritance/src/main/scala/inheritance/Polygon.scala
+++ scala/scala_inheritance/src/main/scala/inheritance/Polygon.scala
@@ -1,20 +1,20 @@
 package Inheritance
 
-class Polygon {
+abstract class Polygon {
     def area: Double = 0.0
 }
 
 object Polygon {
     def main(args: Array[String]) = {
-        var poly = new Polygon;
         var rect = new Rectangle(12, 15)
         var triangle = new Triangle(12, 15)
-        printArea(poly)
         printArea(rect)
         printArea(triangle)
     }
 
     def printArea(p: Polygon) {
-        println(p.area)
+        val this_area = p.area
+        val class_name = p.getClass.getName
+        println(s"area of class $class_name is $this_area")
     }
 }
