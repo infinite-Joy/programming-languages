896c049d77addefeb1ea561ef005cec94a8cf28c
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-03-13 08:28:51 +0530

    scala hello world on sbt

---
+++ scala/hello-world/readme.md
@@ -0,0 +1,1 @@
+https://docs.scala-lang.org/getting-started/sbt-track/getting-started-with-scala-and-sbt-on-the-command-line.html
---
+++ scala/hello-world/src/main/scala/Main.scala
@@ -0,0 +1,5 @@
+object Main extends App {
+  println("Hello joydeep!")
+  println("semicolon is not needed.")
+  println("running using vim")
+}
