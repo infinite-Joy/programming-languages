7b61b0a209b777309fe941e9ed79960ab74d282b
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-03-13 11:42:02 +0530

    scala hello world string interpolation

--- scala/hello-world/src/main/scala/Main.scala
+++ scala/hello-world/src/main/scala/Main.scala
@@ -2,4 +2,6 @@ object Main extends App {
   println("Hello joydeep!")
   println("semicolon is not needed.")
   println("running using vim")
+  val name = "joydeep"
+  println(s"My name is $name")
 }
