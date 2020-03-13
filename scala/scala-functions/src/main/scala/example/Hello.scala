0310f1f9d74e18c4655f06c2a530495cba9a4bf6
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-03-13 12:47:26 +0530

    scala higher order and partial functions

--- scala/scala-functions/src/main/scala/example/Hello.scala
+++ scala/scala-functions/src/main/scala/example/Hello.scala
@@ -1,13 +1,32 @@
 package example
 
+import java.util.Date
+
 object Hello {
   def add(x: Int, y: Int) = x + y;
   def subtract(x: Int, y: Int) = x - y;
   def multiply(x: Int, y: Int) = x * y;
   def divide(x: Int, y: Int) = x / y;
+  def subtract_double(x: Double, y: Double) = x - y;
 
-  def main(args: Array[String]):T = {
-    println(add(45, 15))
+  // higher order functions
+  def math(x: Double, y: Double, f: (Double, Double) => Double) = f(x, y);
+
+  // partially applied function
+  def log(date: Date, message: String) = {
+    println(date + " " + message);
+  }
+
+  def main(args: Array[String]) = {
+    println("computation for simple add function: " + add(45, 15))
+    println("computation for subtract_double: " + math(45.1, 15.1, subtract_double))
+
+    val date = new Date;
+    val newLog = log(date, _: String)
+    newLog("the message 1")
+    newLog("the message 2")
+    newLog("the message 3")
+    newLog("the message 4")
   }
 }
 
