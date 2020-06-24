415907687ed1893d902f79cfa01d45bc5e470d55
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-06-24 08:53:39 +0530

    better stopping criteria for matrix spiral

--- python-projects/hackerrank/matrix_spiral.py
+++ python-projects/hackerrank/matrix_spiral.py
@@ -2,31 +2,33 @@ from collections import defaultdict
 
 def print_matrix(mat, first_row, first_col, last_row, last_col, visited):
     # stopping condition
-    if len(visited) >= len(mat) * len(mat[0]):
+    #if len(visited) >= len(mat) * len(mat[0]):
+    #    return
+    if first_row > last_col or first_col > last_col:
         return
 
     # right -> left
     for j in range(first_col, last_col+1):
-        visited[(first_row, j)] = True
+        #visited[(first_row, j)] = True
         print(mat[first_row][j])
     first_row += 1
 
     # top -> down
     for j in range(first_row, last_row+1):
-        visited[(j, last_col)] = True
+        #visited[(j, last_col)] = True
         print(mat[j][last_col])
     last_col -= 1
 
     # right -> left
     for j in range(last_col, first_col-1, -1):
-        visited[(last_row, j)] = True
+        #visited[(last_row, j)] = True
         #print(visited)
         print(mat[last_row][j])
     last_row -= 1
 
     # bottom -> top
     for j in range(last_row, first_row-1, -1):
-        visited[(j, first_col)] = True
+        #visited[(j, first_col)] = True
         print(mat[j][first_col])
     first_col += 1
 
