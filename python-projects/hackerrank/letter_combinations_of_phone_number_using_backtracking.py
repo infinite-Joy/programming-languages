86e1167323d3652e31c26f8911b9422c13bc2ace
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-07-17 21:57:34 +0530

    letter combinations of phone number using backtraking

--- python-projects/hackerrank/letter_combinations_of_phone_number_using_backtracking.py
+++ python-projects/hackerrank/letter_combinations_of_phone_number_using_backtracking.py
@@ -46,3 +46,25 @@ def letter_combs(digits):
     return solution
 
 print(letter_combs("23"))
+
+############
+
+# better solution
+
+from functools import reduce
+
+class Solution:
+    # @return a list of strings, [s1, s2]
+    def letterCombinations(self, digits):
+        if '' == digits: return []
+        kvmaps = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        return reduce(lambda acc, digit: [x + y for x in acc for y in kvmaps[digit]], digits, [''])
