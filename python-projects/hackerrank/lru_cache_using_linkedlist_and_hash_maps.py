c934a032e483ebb0c953bf5699a4736142ea2bd4
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-07-18 15:59:40 +0530

    lru cache logic simpler - ordered dict is linkedlisthasmap implementation in python

--- python-projects/hackerrank/lru_cache_using_linkedlist_and_hash_maps.py
+++ python-projects/hackerrank/lru_cache_using_linkedlist_and_hash_maps.py
@@ -51,9 +51,6 @@ class LRU:
         if k not in self.mapping:
             return -1
         node = self.mapping[k]
-        if k == 4:
-            #__import__('pdb').set_trace()
-            pass
 
         # if node is the end then nothing to do
         if node == self.end: pass
@@ -94,6 +91,7 @@ class LRU:
                 node.parent = self.end
                 self.end = node
             self.size += 1
+            # size reaches the max capcity
             if self.size > self.capacity:
                 start = self.start
                 del self.mapping[start.key]
@@ -112,6 +110,49 @@ class LRU:
         return "->".join([str((x.key, x.value)) for x in self.print_items()])
 
 cache = LRU(2)
+cache.put(1,1)
+cache.put(2,2)
+print(cache.get(1))
+cache.put(3,3)
+print(cache.get(2))
+cache.put(4,4)
+print(cache.get(1))
+print(cache.get(3))
+print(cache.get(4))
+
+# for a linked hash map implementation collections.ordereddict can  be used
+
+from collections import OrderedDict
+
+
+class LRU_new:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.mapping = OrderedDict()
+
+    def get(self, key):
+        # complexity O(1)
+        if key not in self.mapping:
+            return -1
+        value = self.mapping[key]
+        self.mapping.move_to_end(key)
+        return value
+
+    def put(self, key, value):
+        # complexity O(1)
+        if key in self.mapping:
+            self.mapping.move_to_end(key)
+        self.mapping[key] = value
+        if len(self.mapping) > self.capacity:
+            oldest = next(iter(self.mapping))
+            del self.mapping[oldest]
+
+    def __str__(self):
+        return "->".join([str((k, v)) for k, v in self.mapping.items()])
+
+
+print("with the new lru")
+cache = LRU_new(2)
 cache.put(1,1)
 cache.put(2,2)
 print(cache.get(1))
