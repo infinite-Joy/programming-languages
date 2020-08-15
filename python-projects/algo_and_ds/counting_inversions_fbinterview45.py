ab3d0204bf6a70e6483afdde01321ff04963bfb2
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-08-15 21:01:22 +0530

    counting inversions using array sorting

---
+++ python-projects/algo_and_ds/counting_inversions_fbinterview45.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+# Complete the countInversions function below.
+# go forward. if the present is less than the previous one then the
+#     do a while loop and then find the lower value and then do a swap.
+# 2 1 3 1 2
+#     _
+
+def countInversions(arr):
+    count = 0
+    for idx in range(1, len(arr)):
+        prev = idx - 1
+        if prev >= 0 and arr[idx] < arr[prev]:
+            while prev >= 0 and arr[idx] < arr[prev]:
+                arr[idx], arr[prev] = arr[prev], arr[idx]
+                count += 1
+                idx -= 1
+                prev -= 1
+                print(arr)
+    return count
+
+arr = [2,1,3,1,2]
+print(countInversions(arr))
