5ea44ee5a3933e823f9f00842b4caae01cd53620
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-08-21 09:15:07 +0530

    segment trees perform range queries

--- python-projects/algo_and_ds/segment_trees.py
+++ python-projects/algo_and_ds/segment_trees.py
@@ -37,7 +37,29 @@ def main(arr):
     segmenttree = [inf for _ in range(segmenttree_size)]
     construct_tree(arr, segmenttree, 0, len(arr)-1, 0)
     print(segmenttree)
+    return segmenttree
 
-main(arr)
+segmenttree = main(arr)
+
+# now how to do a range query on  the segment tree
+
+def range_min_query(segmenttree: List[int], query_low: int, query_high: int, low: int, high: int, pos: int) -> int:
+    """
+    time complexity of this range query is O(logn)
+    """
+    print(segmenttree, query_low, query_high, low, high, pos)
+    if query_low <= low and query_high >= high: # total overlap
+        return segmenttree[pos]
+    if query_low > high or query_high < low: # no overlap
+        return inf
+    mid = (low+high)//2
+    return min(
+        range_min_query(segmenttree, query_low, query_high, low, mid, 2*pos+1),
+        range_min_query(segmenttree, query_low, query_high, mid+1, high, 2*pos+2),
+    )
+
+range_min = range_min_query(segmenttree, 1, 3, 0, len(arr)-1, 0)
+print(range_min)
+
 
 
