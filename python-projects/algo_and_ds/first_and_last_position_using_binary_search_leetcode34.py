e2b37ca632cfc9de19831ac45eaffb872ff7b8a3
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-08-22 08:54:19 +0530

    first and last position using binary seach

---
+++ python-projects/algo_and_ds/first_and_last_position_using_binary_search_leetcode34.py
@@ -0,0 +1,67 @@
+from typing import List
+
+def find_left(nums, target):
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            import time; time.sleep(0.5)
+            print(low, high)
+            if low == high or low + 1 == high:
+                if nums[low] == target:
+                    return low
+                if nums[high] == target:
+                    return high
+                else:
+                    return -1
+            mid = (low + high) // 2
+            if nums[mid] < target:
+                low = mid
+            else:
+                high = mid
+def find_right(nums, target):
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            import time; time.sleep(0.5)
+            print(low, high)
+            if low == high or low + 1 == high:
+                if nums[low] == target:
+                    return low
+                if nums[high] == target:
+                    return high
+                else:
+                    return -1
+            mid = (low + high) // 2
+            if nums[mid] <= target:
+                low = mid
+            else:
+                high = mid
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # we can probably do this in 2 passes
+        # first we find the left one then we do the right one
+        # left one
+        # time complexity : O(logn)
+        if len(nums) == 0:
+            return -1
+        if len(nums) == 1:
+            if nums[0] == target:
+                return [0, 0]
+            else:
+                return -1
+
+        left = find_left(nums, target)
+        right = find_right(nums, target)
+
+        return [left, right]
+
+s = Solution()
+nums = [5,7,7,8,8,10]
+target = 8
+print(s.searchRange(nums, target))
+
+nums = [5,7,7,8,8,10]
+target = 6
+print(s.searchRange(nums, target))
+
+
