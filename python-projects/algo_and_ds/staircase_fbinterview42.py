947a1d89e0e005324b85c515baf7178ae63eae11
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-08-24 08:17:12 +0530

    staircase using finbonacci

--- python-projects/algo_and_ds/staircase_fbinterview42.py
+++ python-projects/algo_and_ds/staircase_fbinterview42.py
@@ -1,36 +1,39 @@
-BACKTRACk = 0
-def find_values(n, values, summ, count, possibilities):
-    global BACKTRACk
-    print(n, values, summ, count)
-    for i in values:
-        summ += i
-        possibilities.append(i)
-        if sum(possibilities) == n:
-            print(possibilities)
-        if sum(possibilities) < n:
-            count += find_values(n, values, summ, count, possibilities)
-            possibilities.pop()
-            BACKTRACk += 1
-            summ -= i
-        else:
-            return count
-    return count
+"""
+This is a fibonacci problem and using dp to solve it.
 
+time complexity: O(n)
+space: O(n) this can be reduced to O(1)
+
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+# Complete the stepPerms function below.
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    dp = [0 for _ in range(n+1)]
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        print(dp)
+
+    return dp[n]
 
 def stepPerms(n):
-    values = list(range(1, n+1))
-    count = find_values(n, values, 0, 0, [])
-    return count
+    return fibonacci(n)
 
-
-print('ans', stepPerms(1))
-print(BACKTRACk)
-BACKTRACk = 0
-
-print('ans', stepPerms(2))
-print(BACKTRACk)
-BACKTRACk = 0
-
-#print('ans', stepPerms(3))
-#print(BACKTRACk)
-#BACKTRACk = 0
