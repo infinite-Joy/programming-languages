0862a7580e2bc9c292dbe5e0a30932fac9e813f6
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2020-10-11 11:02:56 +0530

    find prime factors based on geeks for geeks

---
+++ python-projects/algo_and_ds/prime_factoras.py
@@ -0,0 +1,29 @@
+# find the prime factors based on the geeks for geeks tut
+
+from math import sqrt
+def is_prime(n):
+    if n == 1: return 1
+    if n == 2: return True
+    if n == 3: return True
+
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    for i in range(5, int(sqrt(n)), 6):
+        if (n % i == 0) or (n % (i + 2) == 0):
+            return False
+    return True
+
+def print_prime_factors(n):
+    print("printing prime factors of {}".format(n))
+    if n <= 1:
+        return
+
+    i = 2
+    while n >= i * i:
+        if is_prime(i):
+            while n % i == 0:
+                print(i)
+                n /= i
+        i += 1
+
+print_prime_factors(450)
