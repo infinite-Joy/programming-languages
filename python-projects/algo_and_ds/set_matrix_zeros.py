ac6a7a9e2fb193db73c163cba0b89c7c7c9e6ab7
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2022-05-13 11:54:16 +0530

    set matrix zeros using matrix understanding

---
+++ python-projects/algo_and_ds/set_matrix_zeros.py
@@ -0,0 +1,33 @@
+"""
+create a arr of row and cols
+get the multiplication of the rows and cols
+whichever is 0 make it 0
+
+
+"""
+
+import operator
+from functools import reduce
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        
+        this is an O(n+m) space solution
+        """
+        rows = [1] * len(matrix)
+        cols = [1] * len(matrix[0])
+        for i, r in enumerate(matrix):
+            rows[i] = reduce(operator.mul, r, 1)
+        for i, c in enumerate(matrix[0]):
+            cols[i] = reduce(lambda x, y: operator.mul(x, y[i]), matrix, 1)
+            
+        # make all rows 0
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if rows[r] == 0 or cols[c] == 0:
+                    matrix[r][c] = 0
+                    
+        return matrix
